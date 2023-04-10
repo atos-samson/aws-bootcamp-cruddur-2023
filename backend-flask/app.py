e9fb2c58160c649446e2b506a92117f52bf8b1bd
Samson Oyetayo <samson.oyetayo.external@atos.net>
2023-04-10 23:06:50 +0000

    implemented rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,10 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 import watchtower
 import logging
 from time import strftime
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 # HoneyComb ---------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -153,6 +157,27 @@ def data_activities_reply(activity_uuid):
   else:
     return model['data'], 200
   return
+
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 if __name__ == "__main__":
   app.run(debug=True)
